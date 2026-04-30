771c95a25fd4c874d6f2aa5677815eb658edc6ba
PapiCruz <150858271+PapiCruz@users.noreply.github.com>
2026-04-29 22:12:33 -0600

    Add files via upload

---
+++ HashGenerator.cpp
@@ -0,0 +1,19 @@
+#include "HashGenerator.h"
+#include <openssl/sha.h>
+#include <sstream>
+#include <iomanip>
+
+std::string generateSHA256(const std::vector<unsigned char>& data) {
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+
+    SHA256(data.data(), data.size(), hash);
+
+    std::stringstream ss;
+
+    for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
+        ss << std::hex << std::setw(2) << std::setfill('0')
+           << static_cast<int>(hash[i]);
+    }
+
+    return ss.str();
+}
